b09a19a9b3fb8f78da0a728331a60b2bf1c94fad
grammophone <emmanuel.liossis@gmail.com>
2017-01-24 01:23:29 +0200

    Add marking interface for user tracking entity listeners.

---
+++ IUserTrackingEntityListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grammophone.DataAccess;
+using Grammophone.Domos.Domain;
+
+namespace Grammophone.Domos.DataAccess
+{
+	/// <summary>
+	/// Marker interface for entity listeners
+	/// updating <see cref="TrackingEntity{U}.CreatorUser"/>,
+	/// <see cref="TrackingEntity{U}.CreationDate"/>, 
+	/// <see cref="TrackingEntity{U}.LastModifierUser"/>
+	/// <see cref="TrackingEntity{U}.LastModificationDate"/>
+	/// and <see cref="UserTrackingEntity{U}.OwningUser"/>.
+	/// </summary>
+	public interface IUserTrackingEntityListener : IEntityListener
+	{
+	}
+}
